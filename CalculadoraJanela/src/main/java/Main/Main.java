9667647da33a4dba5a7392ea8186165b676a55b6
Hiago de Mendonça <163073487+HiagoDeMendonca@users.noreply.github.com>
2024-04-29 23:48:12 -0300

    Create Project
    Export project from NetBeans IDE

---
+++ CalculadoraJanela/src/main/java/Main/Main.java
@@ -0,0 +1,14 @@
+package Main;
+
+import View.FrmCalculadora;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        FrmCalculadora janela = new FrmCalculadora();
+        janela.setVisible(true);
+        
+    }
+    
+}
