27b1572558a4f9cc06b01a16e0af82b4222780d8
Hiago de Mendonça <163073487+HiagoDeMendonca@users.noreply.github.com>
2024-05-06 19:39:03 -0300

    Reposição dos arquivos
    Reposição dos arquivos originais fora da pasta CalculadoraJanela
    Exclusão da pasta target

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,16 @@
+package Main;
+
+import View.FrmCalculadora;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+		System.out.println("adfasdasd");
+		
+        FrmCalculadora janela = new FrmCalculadora();
+        janela.setVisible(true);
+        
+    }
+    
+}
